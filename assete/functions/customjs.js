001c25b89cae11f8487f1a7e82654bfa5317982f
monish <monishbarse9@gmail.com>
2023-04-25 00:54:50 +0530

    form

--- assete/functions/customjs.js
+++ assete/functions/customjs.js
@@ -13,3 +13,14 @@ counters.forEach((counter) => {
   };
   updateCounter();
 });
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzrK_n8oKI6pyRlmgspA2RqVAbHL6kYn02Av9mQ-xlwwRshC8RSTqv8xavkL9koWN_mYQ/exec'
+const form = document.forms['google-sheet']
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .catch(error => console.error('Error!', error.message))
+})
